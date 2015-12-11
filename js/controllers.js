b8b1260fe926de619689b59da0fb1afc1178aa73
Tomas <me@tomasre.com>
2015-12-10 22:15:10 -0800

    basic card view with some of reddit elements

--- js/controllers.js
+++ js/controllers.js
@@ -1,11 +1,17 @@
 'use strict';
 
 angular.module('touchReddit.controllers', [])
-.controller('HomeCtrl', ['$scope', 'apiWrapper', function ($scope, apiWrapper) {
+.controller('HomeCtrl', ['$scope', 'apiWrapper', '$window', function ($scope, apiWrapper, $window) {
 
     // holds the data from the reddit api
     $scope.apiData;
     apiWrapper.get().then(function (response) {
         $scope.apiData = response;
     });
+
+    // change the url to a new url outside of app - for making a md-card a link
+    $scope.changeUrl = function (url) {
+        $window.location = url;
+    };
+
 }]);
